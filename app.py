ce717a47f353fcfa4ec43fbf9b6f776607c5eef0
Mira Hyvönen <mira.hyvonen@iki.fi>
2025-03-18 19:07:49 +0200

    Tietokannan minimaalinen toteutus: arvon hakeminen ja tallentaminen

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, redirect
+import sqlite3
+import tietokanta
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    kayttajaluettelo = tietokanta.kysely("SELECT tunnus FROM kayttajat")
+    kayttajia = len(kayttajaluettelo)
+    return render_template("index.html", n = kayttajia, tunnukset = kayttajaluettelo)
 
+@app.route("/rekisteroidy")
+def rekisteroidy():
+    uusi_tunnus = "Uusi Tunnus"
+    tietokanta.suorita("INSERT INTO kayttajat (tunnus) VALUES (?);", [uusi_tunnus])
+    return redirect("/")
