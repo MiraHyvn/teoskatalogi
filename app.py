dff3a817307711e4d00dabbeb6c17a07129905bf
Mira Hyvönen <mira.hyvonen@iki.fi>
2025-03-24 18:09:06 +0200

    Käyttäjätunnuksen luominen, kirjautuminen ja sessiomuuttuja

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
-from flask import redirect, render_template, request
-from werkzeug.security import generate_password_hash
+from flask import redirect, render_template, request, session
+from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
 import tietokanta
+import config
 
 app = Flask(__name__)
+app.secret_key = config.salainen_avain
 
 # Tietokannassa tietokanta.db on oltava taulu, joka on luotu esim näin:
 # CREATE TABLE kayttajat (id INTEGER PRIMARY KEY, tunnus TEXT UNIQUE, salasana_hash TEXT);
@@ -17,8 +19,22 @@ def index():
 
 @app.route("/rekisteroidy")
 def rekisteroidy():
+    # Pitäisikö olla mahdollista kun ollaan kirjautuneena?
     return render_template("rekisteroidy.html")
 
+@app.route("/kirjaudu", methods=["POST"])
+def kirjaudu():
+    # Ongelma: Jos käyttäjä syöttää tunnuksen, jota ei ole => Internal server error (500)
+    annettu_tunnus = request.form["tunnus"]
+    annettu_salasana = request.form["salasana"]
+    sql = "SELECT salasana_hash FROM kayttajat WHERE tunnus = ?"
+    oikea_hash = tietokanta.kysely(sql, [annettu_tunnus])[0][0]
+    if check_password_hash(oikea_hash, annettu_salasana):
+        session["kayttajatunnus"] = annettu_tunnus
+        return redirect("/")
+    else:
+        return "Virhe: Väärä tunnus tai salasana." 
+    
 @app.route("/luo_kayttaja", methods=["POST"])
 def luo_kayttaja():
     uusi_tunnus = request.form["tunnus"]
@@ -30,4 +46,8 @@ def luo_kayttaja():
     except sqlite3.IntegrityError:
         return "virhe"
     return redirect("/")
-    
+
+@app.route("/kirjaudu_ulos")
+def kirjaudu_ulos():
+    del session["kayttajatunnus"]
+    return redirect("/")
---
+++ config.py
@@ -0,0 +1,2 @@
+# tehtävä myöhemmin: Avaimesta pitäisi tehdä oikeasti salainen - instance folder
+salainen_avain = "f18b2d2f8f2d5e17f339611daf66207b"
