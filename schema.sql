979c010d6bc2670b903d8f7badca27c27de1d7d8
Mira Hyvönen <mira.hyvonen@iki.fi>
2025-03-24 18:32:30 +0200

    Tietokannan rakenne schema.sql-tiedostosta

---
+++ schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE kayttajat (
+	id INTEGER PRIMARY KEY,
+	tunnus TEXT UNIQUE, 
+	salasana_hash TEXT
+);
+
+CREATE TABLE teokset (
+	id INTEGER PRIMARY_KEY,
+	nimi TEXT,
+    	kayttaja_id INTEGER REFERENCES kayttajat	
+);
+
