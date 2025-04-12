61a7a1e848eb8014adfdfc916112c9103c1c3b9d
Mira Hyvönen <mira.hyvonen@iki.fi>
2025-04-12 14:37:43 +0300

    Added classes to schema.sql. Initialization of classes: init.sql

---
+++ init.sql
@@ -0,0 +1,10 @@
+INSERT INTO Classes (title, value) VALUES ("medium", "painting");
+INSERT INTO Classes (title, value) VALUES ("medium", "sculpture");
+INSERT INTO Classes (title, value) VALUES ("medium", "video");
+INSERT INTO Classes (title, value) VALUES ("medium", "performance");
+INSERT INTO Classes (title, value) VALUES ("medium", "installation");
+INSERT INTO Classes (title, value) VALUES ("medium", "drawing");
+INSERT INTO Classes (title, value) VALUES ("medium", "print");
+INSERT INTO Classes (title, value) VALUES ("medium", "digital");
+INSERT INTO Classes (title, value) VALUES ("medium", "other");
+
--- schema.sql
+++ schema.sql
@@ -17,7 +17,20 @@ CREATE TABLE Collections (
 );
 
 CREATE TABLE WorksInCollection (
-	id integer PRIMARY KEY,
+	id INTEGER PRIMARY KEY,
 	work_id INTEGER REFERENCES Works,
 	collection_id INTEGER REFERENCES Collections
 );
+
+CREATE TABLE Classes (
+	id INTEGER PRIMARY KEY,
+	title TEXT,
+	value TEXT
+);
+
+CREATE TABLE WorkClasses (
+	id INTEGER PRIMARY KEY,
+	work_id INTEGER REFERENCES Works,
+	class_id INTEGER REFERENCES Classes
+);
+
